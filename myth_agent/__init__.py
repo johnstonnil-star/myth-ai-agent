f812975c315125b94ce551371fc8df74dd45e948
johnstonnil-star <johnston.nil@gmail.com>
2026-04-08 22:21:07 -0300

    Create __init__.py

---
+++ myth_agent/__init__.py
@@ -0,0 +1,5 @@
+# myth_agent/__init__.py
+from .myth_agent import MythAgent
+from .memory import MemoryLayer
+from .tools import myth_generator
+from .myth_langchain_agent import build_langchain_myth_agent
